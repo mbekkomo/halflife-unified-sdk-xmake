89f71f788d1dcbe93c24cae58c5e386c7e594f17
Komo <afiqquraisyzulkarnain@gmail.com>
2025-01-18 19:23:36 +0800

    init nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Half-Life Updated SDK with Opposing Force and Blue Shift merged in, along with other improvements. Check README.md for more information. (Using XMake build tool)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        inherit (pkgs) stdenv;
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          buildInputs = with pkgs; [ xmake ];
+        };
+      }
+    );
+}
